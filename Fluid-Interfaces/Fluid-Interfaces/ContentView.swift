2e7fccb4cfce3675fd2a613c0fe49cde6e254903
Wayne Dahlberg <waynefj40@gmail.com>
2020-05-18 23:17:59 -0600

    Initial commit
    added Xcode project

---
+++ Fluid-Interfaces/Fluid-Interfaces/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fluid-Interfaces
+//
+//  Created by Wayne Dahlberg on 5/18/20.
+//  Copyright © 2020 Floppy Hat, LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
