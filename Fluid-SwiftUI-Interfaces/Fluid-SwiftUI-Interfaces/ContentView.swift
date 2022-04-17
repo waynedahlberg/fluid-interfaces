4250248774f0d73cd2c132450fabfe31fa46f416
Wayne Dahlberg <waynefj40@gmail.com>
2022-04-16 19:56:31 -0600

    add updated project

---
+++ Fluid-SwiftUI-Interfaces/Fluid-SwiftUI-Interfaces/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fluid-SwiftUI-Interfaces
+//
+//  Created by Wayne Dahlberg on 4/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fluid-SwiftUI-Interfaces/Fluid-SwiftUI-Interfaces/Fluid_SwiftUI_InterfacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fluid_SwiftUI_InterfacesApp.swift
+//  Fluid-SwiftUI-Interfaces
+//
+//  Created by Wayne Dahlberg on 4/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Fluid_SwiftUI_InterfacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
